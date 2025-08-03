a05e0172b3601123711bec1d83f886a6036f570c
Stephen Wike <stephen.c.wise@gmail.com>
2025-08-03 12:24:28 -0600

    bring id back, needed for listing

--- src/types/model/dance.model.ts
+++ src/types/model/dance.model.ts
@@ -2,6 +2,7 @@ import { ChoreographerModel } from './choreographer.model';
 import { TrackModel } from './track.model';
 
 export interface DanceModel {
+    id: string;
     danceName: string;
     choreographers: ChoreographerModel[];
     stepsheet: string;
--- src/types/requests/userInfoUpdate.request.ts
+++ src/types/requests/userInfoUpdate.request.ts
@@ -1,7 +1,6 @@
 import { SocialMediaLinks } from "../model/userTypes/socialMediaLinks";
 
 export interface UserInfoUpdateRequest {
-    userId: string;
     name?: string;
     username?: string;
     profilePictureUrl?: string;
